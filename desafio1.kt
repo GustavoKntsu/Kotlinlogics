87efeb0dd9ef75db759f1f8efd94de513b172bd2
GustavoKntsu <gustavokntsu@gmail.com>
2026-09-04 15:54:06 -0300

    Commit inicial da atividade de desenv. mobile

---
+++ desafio1.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val stringA = "Ola"
+    val stringB = "Mundo"
+
+    if (stringA == stringB) {
+        println("As strings são iguais.")
+    } else {
+        println("As strings são diferentes.")
+    }
+   
+}
